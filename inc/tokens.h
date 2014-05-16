6b8f2763b726711ee9f9829eedcc72636d45b999
nsierra- <nsierra-@student.42.fr>
2014-05-16 06:50:30 +0200

    Normalized

--- inc/tokens.h
+++ inc/tokens.h
@@ -6,18 +6,18 @@
 /*   By: nsierra- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/16 04:06:39 by nsierra-          #+#    #+#             */
-/*   Updated: 2014/05/16 05:02:06 by nsierra-         ###   ########.fr       */
+/*   Updated: 2014/05/16 06:36:51 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENS_H
 # define TOKENS_H
 
-#define TOK_HEREDOC "<<"
-#define TOK_APPEND ">>"
-#define TOK_REDIR_IN "<"
-#define TOK_REDIR_OUT ">"
-#define TOK_PIPE "|"
-#define TOK_SEMICOL ";"
+# define TOK_HEREDOC "<<"
+# define TOK_APPEND ">>"
+# define TOK_REDIR_IN "<"
+# define TOK_REDIR_OUT ">"
+# define TOK_PIPE "|"
+# define TOK_SEMICOL ";"
 
 #endif
